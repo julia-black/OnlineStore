b3eaa3827004cd7a083bdc5817c980b5a8468fa4
julia-black <b-r-cat@ya.ru>
2017-10-07 23:39:18 +0400

    Fixed a few methods

--- src/main/java/src/controller/GoodsController.java
+++ src/main/java/src/controller/GoodsController.java
@@ -3,8 +3,10 @@ package src.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import src.model.Good;
 import src.model.GoodsDAO;
 import sun.rmi.runtime.Log;
@@ -14,6 +16,7 @@ import java.util.List;
 
 @RestController
 public class GoodsController {
+
     @RequestMapping("/create")
     public String create(String name, Integer price) {
 
@@ -42,8 +45,20 @@ public class GoodsController {
         return good;
     }
 
+  //@RequestMapping("/get_by_id")
+  //public Good getById(Integer id) {
+  //    Good good;
+  //    try {
+  //        good = goodsDAO.findById(id);
+  //    }
+  //    catch (Exception ex) {
+  //        return null;
+  //    }
+  //    return good;
+  //}
+
     @RequestMapping("/get_by_id")
-    public Good getById(Integer id) {
+    public String getById(@RequestParam("id") Integer id) {
         Good good;
         try {
             good = goodsDAO.findById(id);
@@ -51,28 +66,28 @@ public class GoodsController {
         catch (Exception ex) {
             return null;
         }
-        return good;
+        return good.getName() + " " + good.getPrice();
     }
-
-    @RequestMapping("/get_all_goods")
+     @RequestMapping("/get_all_goods")
      public List<Good> getAllGoods(){
          List<Good> goods = (List<Good>) goodsDAO.findAll();
          return goods;
      }
 
      @RequestMapping("/add_to_cart")
-     public String addToCart(Integer id){
+     public String addToCart(Integer id) {
          Good good;
-         try {
-             good = goodsDAO.findById(id);
-             //Подключение к лич кабинету, добавление  к нему в корзину good
-         }
-         catch (Exception ex) {
-             return "Good with id = " + id + " not found";
-         }
+             try {
+                 good = goodsDAO.findById(id);
+                 //Подключение к лич кабинету, добавление  к нему в корзину good
+             } catch (Exception ex) {
+                 return "Good with id = " + id + " not found";
+             }
          return "Good + " + good.getName() + "added in Your cart";
-     }
+         }
+
 
     @Autowired
     private GoodsDAO goodsDAO;
+
 }
